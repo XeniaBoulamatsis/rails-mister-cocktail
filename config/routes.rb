f4e849723f5902aed4811e3ad367e2f468127e4f
Xenia Boulamatsis <xenia.boulamatsis@gmail.com>
2020-02-21 09:12:07 -0300

    cocktail1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
 end
+# get '/restaurants', to: 'restaurants#index',
+# post '/restaurants', to: 'restaurants#create',
+# get '/restaurants/new', to: 'restaurants#new',
+# get '/restaurants/:id', to: 'restaurants#show',
+# delete "tasks/:id", to: "tasks#destroy"
